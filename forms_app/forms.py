184a27589fe508773769203d8b82ddf34ce5db2d
Justin Beausoleil <beausoleil.justin@gmail.com>
2020-03-15 22:31:52 -0600

    form validation

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,8 +1,26 @@
 from django import forms
+from django.core import validators
+
+
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        print('uh oh')
+        raise forms.ValidationError("Name needs to start with 'z'")
 
 
 # Create basic form
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput,
+                                 validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email'] # All Clean Data is a dictionary
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError('Ensure emails match')
--- forms_app/views.py
+++ forms_app/views.py
@@ -5,4 +5,11 @@ from . import forms
 # Create your views here.
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print('Validation Success')
+
     return render(request, 'forms_app/form_page.html', {'form': form})
